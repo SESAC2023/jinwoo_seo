469a88878cd01d565e9d883b3a022b417ce75198
서진우 <110076710+dukong1@users.noreply.github.com>
2023-06-20 21:35:07 +0900

    Update 2525.py

--- BOI/st_02_if/2525.py
+++ BOI/st_02_if/2525.py
@@ -4,14 +4,14 @@
 h, m = map(int, input().split())
 t = int(input()) 
 
-h += t // 60 
+h += t // 60
 m += t % 60
-
-if h >= 24:
-    h -= 24
 
 if m >= 60:
     h += 1
     m -= 60
+
+if h >= 24:
+    h -= 24
 
 print(h, m)
