7724a754ce50b54f727601e803e036ebd1c12696
서진우 <110076710+dukong1@users.noreply.github.com>
2023-06-24 10:02:39 +0900

    Update 24479.py

--- BOJ/st_31_graph_traversal/24479.py
+++ BOJ/st_31_graph_traversal/24479.py
@@ -1,9 +1,21 @@
+"""
+<24479>
+1. 내용 훑기 - 그래프, DFS, 방문처리, 탐색
+2.(중요) 입력 조건:
+-노드(도시) 혹은 정점 개수 최대 100,000개
+-간선(도로) 개수 최대 200,000개
+3. 문제 아이디어 생각하기
+DFS로 풀면 되지 않을까?
+DFS가 시간 복잡도가 O(M+N)이니까, 단순 DFS로도 가능
+"""
+
 import sys
 # 재귀 깊이 한도 해제
 sys.setrecursionlimit(int(1e6))
 # 빠르게 입력받기
 input = sys.stdin.readline
 
+# n(노드개수), m(간선개수), r(시작노드)
 n, m, r = map(int, input().split())
 """
 graph = [
@@ -21,6 +33,7 @@ answer = [0] * (n + 1)
 
 for i in range(m):
     u, v = map(int, input().split())
+    # graph[u].append(v) / graph[v].append(u) -> 무방향 그래프 = 양방향 그래프 , 방향이 한쪽이면 graph[u].append(v) 만 하면 됨.
     graph[u].append(v)
     graph[v].append(u)
 
