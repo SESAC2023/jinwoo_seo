15c208c97ef0fa9b595b32941e401d5cd84c0fdd
서진우 <110076710+dukong1@users.noreply.github.com>
2023-07-24 22:35:45 +0900

    Update 11050.py

--- BOJ/st_17_combinatorics/11050.py
+++ BOJ/st_17_combinatorics/11050.py
@@ -1,1 +1,26 @@
+# n! / k!(n-k!)
 
+import sys
+input = sys.stdin.readline
+# for문
+def factorial(n):
+    if n == 0:
+        return 1
+      
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+n, k = map(int, input().split())
+print(factorial(n) // (factorial(k) * factorial(n - k)))
+
+# 재귀문
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+n, k = map(int, input().split())
+print(factorial(n) // (factorial(k) * factorial(n - k)))
